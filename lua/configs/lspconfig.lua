7103f1fc9974b2396698fa07f75c51c3cecc33cb
JingpengZhang <jingpeng_zhang@foxmail.com>
2024-07-08 17:34:20 +0800

    update

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "vtsls",'tailwindcss',"volar" }
+local servers = { "html", "cssls", "vtsls",'tailwindcss',"vuels" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
